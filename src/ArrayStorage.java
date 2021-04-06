581e8ff2a63176d2f9d7ea97931f3e5363d4ed91
BanForName <57586598+BanForName@users.noreply.github.com>
2021-04-06 16:55:21 +0500

    Уже пора домой. закончу дома)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,38 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid)) return resume;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (Resume resume : storage) {
+            
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (Resume resume : storage) {
+            if (!(resume == null)) size++;
+        }
+        return size;
     }
 }
