f8e9d8761b64abe559791d7c58410e3386c1694e
BanForName <57586598+BanForName@users.noreply.github.com>
2021-04-08 08:37:50 +0500

    Added a cyclic shift to the delete method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,51 +5,51 @@ import java.util.Comparator;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    private int size;
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) return storage[i];
         }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size(); i++) {
-            if (storage[i].uuid.equals(uuid)) storage[i] = null;
-        }
-        
-        Arrays.sort(storage, new Comparator<Resume>() {
-            @Override
-            public int compare(Resume o1, Resume o2) {
-                if (o1 == null && o2 == null) return 0;
-                if (o1 == null) return 1;
-                if (o2 == null) return -1;
-                return o1.uuid.compareTo(o2.uuid);
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                break;
             }
-        });
+        }
+        size--;
+
+        for (int i = 0; i < size; i++) {
+            if (storage[i] == null) {
+                storage[i] = storage[i + 1];
+                storage[i + 1] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume : storage) {
-            if (resume != null) size++;
-        }
         return size;
     }
 }
