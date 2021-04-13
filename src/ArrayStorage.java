b878d8d94dbf685f8d04030dc1c5e1c152e3eea2
BanForName <revan931@yandex.ru>
2021-04-13 15:26:37 +0500

    re delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -35,8 +35,8 @@ public class ArrayStorage {
             }
         }
 
-        for (int i = 0; i < size; i++) {
-            storage[index + i] = storage[index + i + 1];
+        for (int i = index; i < size; i++) {
+            storage[i] = storage[i + 1];
         }
     }
 
