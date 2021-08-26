3aa17227d91fc30feb2e4027421d4ecaf4b8ce92
BanForName <revan931@yandex.ru>
2021-08-26 16:53:02 +0500

    enum resume section type

--- src/com/TestSingleton.java
+++ src/com/TestSingleton.java
@@ -1,13 +1,31 @@
 package com;
 
-public class TestSingleton {
-    private static final TestSingleton istance = new TestSingleton();
+import com.topjava.webapp.model.SectionType;
 
-    public static TestSingleton getInstance() {
-        return istance;
-    }
+public class TestSingleton {
+    private static TestSingleton instance;
 
     private TestSingleton() {
+    }
 
+    public static TestSingleton getInstance() {
+        if (instance == null) {
+            instance = new TestSingleton();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        TestSingleton.getInstance().toString();
+        Singleton instance = Singleton.valueOf("INSTANCE");
+        System.out.println(instance.ordinal());
+
+        for (SectionType type : SectionType.values()) {
+            System.out.println(type.getTitle());
+        }
+    }
+
+    public enum Singleton {
+        INSTANCE
     }
 }
---
+++ src/com/topjava/webapp/model/SectionType.java
@@ -0,0 +1,20 @@
+package com.topjava.webapp.model;
+
+public enum SectionType {
+    PERSONAL("Личные качества"),
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт раьботы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
