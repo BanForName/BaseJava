b9f554d0c8474b370ba3ebb94b04c32089a86472
BanForName <revan931@yandex.ru>
2021-09-15 14:09:34 +0500

    HW7 MapResumeStorage

---
+++ src/com/topjava/webapp/storage/mapStorage/MapResumeStorage.java
@@ -0,0 +1,58 @@
+package com.topjava.webapp.storage.mapStorage;
+
+import com.topjava.webapp.model.Resume;
+import com.topjava.webapp.storage.AbstractStorage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapResumeStorage extends AbstractStorage<Resume> {
+    private Map<String, Resume> resumeMap = new HashMap<>();
+
+    @Override
+    protected void updateResume(Resume resume, Resume searchKey) {
+        resumeMap.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    protected void saveResume(Resume resume, Resume searchKey) {
+        resumeMap.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    protected Resume getResume(Resume searchKey) {
+        return searchKey;
+    }
+
+    @Override
+    protected void deleteResume(Resume searchKey) {
+        resumeMap.remove(searchKey.getUuid());
+    }
+
+    @Override
+    protected boolean isExist(Resume searchKey) {
+        return searchKey != null;
+    }
+
+    @Override
+    protected Resume getKey(String uuid) {
+        return resumeMap.get(uuid);
+    }
+
+    @Override
+    public void clear() {
+        resumeMap.clear();
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        return new ArrayList<>(resumeMap.values());
+    }
+
+    @Override
+    public int size() {
+        return resumeMap.size();
+    }
+}
---
+++ test/com/topjava/webapp/storage/MapResumeStorageTest.java
@@ -0,0 +1,9 @@
+package com.topjava.webapp.storage;
+
+import com.topjava.webapp.storage.mapStorage.MapResumeStorage;
+
+public class MapResumeStorageTest extends AbstractStorageTest {
+    public MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
