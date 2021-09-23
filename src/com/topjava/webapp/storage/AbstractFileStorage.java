1c2a8c6452fde6313285fff99531bcc400dcf806
BanForName <revan931@yandex.ru>
2021-09-23 09:35:10 +0500

    L8 AbstractFileStorage

---
+++ src/com/topjava/webapp/storage/AbstractFileStorage.java
@@ -0,0 +1,76 @@
+package com.topjava.webapp.storage;
+
+import com.topjava.webapp.exception.StorageException;
+import com.topjava.webapp.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private File directory;
+
+    public AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory + " must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    protected void updateResume(Resume resume, File file) {
+
+    }
+
+    @Override
+    protected void saveResume(Resume resume, File file) {
+        try {
+            file.createNewFile();
+            resumeWrite(resume, file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    protected abstract void resumeWrite(Resume resume, File file) throws  IOException;
+
+    @Override
+    protected Resume getResume(File file) {
+        return null;
+    }
+
+    @Override
+    protected void deleteResume(File file) {
+
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected File getKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
