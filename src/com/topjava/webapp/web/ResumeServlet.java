e52b0226d464b34c8eac78c49c62d6276ed3f105
BanForName <revan931@yandex.ru>
2022-02-24 15:12:09 +0500

    resume table in servlet

--- src/com/topjava/webapp/web/ResumeServlet.java
+++ src/com/topjava/webapp/web/ResumeServlet.java
@@ -1,12 +1,25 @@
 package com.topjava.webapp.web;
 
+import com.Config;
+import com.topjava.webapp.model.Resume;
+import com.topjava.webapp.storage.Storage;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.Writer;
 
 @WebServlet("/resume")
 public class ResumeServlet extends HttpServlet {
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getStorage();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
@@ -15,7 +28,28 @@ public class ResumeServlet extends HttpServlet {
 //        response.setHeader("Content-type", "text/html; charset=UTF-8");
 //        String name = request.getParameter("name");
 //        response.getWriter().write(name == null ? "<h1>Hello world!</h1>" : "Hello " + name);
-        request.getRequestDispatcher("web/test.html").forward(request, response);
+        Writer writer = response.getWriter();
+        writer.write(
+                "<html>" +
+                        "<head>" +
+                        "   <title>All resumes</title>" +
+                        "</head>" +
+                        "<body>" +
+                        "   <table border='1'>" +
+                        "        <tr>" +
+                        "           <th>uuid</th>" +
+                        "           <th>full name</th>" +
+                        "       </tr>");
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write("<tr>" +
+                            "   <td>" + resume.getUuid() + "</td>" +
+                            "   <td>" + resume.getFullName() + "</td>" +
+                            "</tr>");
+        }
+        writer.write("</table>" +
+                        "</body>" +
+                "</html>");
+
     }
 
     @Override
